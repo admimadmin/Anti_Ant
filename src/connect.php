4a8f2dbeb217c76ba4a32bbec9c62e2457cc5dcd
admin toor <d.developerlastname@yandex.com>
2022-02-05 19:12:24 -0800

    create new file connection database config

--- src/connect.php
+++ src/connect.php
@@ -5,10 +5,7 @@
 /****/
 function connect(){
 
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'work_primade');
+    require_once('config.php');
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
     return $db;
 }
